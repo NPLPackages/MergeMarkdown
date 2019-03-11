59d8786f9e847dbdb560c82d70a2adf189983652
LiXizhi <lxz1982@hotmail.com>
2019-03-11 12:25:37 +0800

    v1.0 is done

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # MergeMarkdown
-merge a set of markdown files into one according to headers
+merge a set of markdown files into one according to special notations.
+
+
+A special syntax: `<<[]()` is used for merging to take place recursively from the root document. 
+
+```
+## root markdown file
+
+<<[merge point1](https://keepwork.com/lixizhi/lessons/books/paracraft_01)
+<<[merge point2](lixizhi/lessons/books/paracraft_02)
+
+```
+
+
+## how to use the lib:
+git clone to `npl_packages/` folder and then run:
+
+```lua
+local MergeMarkdown = NPL.load("MergeMarkdown");
+-- output file in "temp/MergedMarkdown.md"
+-- example 1:
+local mmd = MergeMarkdown:new():Init("https://keepwork.com/lixizhi/lessons/books/paracraft01")
+```
--- npl_mod/MergeMarkdown/MergeMarkdown.lua
+++ npl_mod/MergeMarkdown/MergeMarkdown.lua
@@ -3,7 +3,7 @@ Title: Merge Markdown
 Author(s): LiXizhi@yeah.net
 Date: 2019/3/8
 Desc: 
-A special context: `<<[]()` is required for merging to take place. 
+A special context: `<<[]()` is used for merging to take place recursively from the root document. 
 
 ```
 ## root markdown file
@@ -16,19 +16,31 @@ A special context: `<<[]()` is required for merging to take place.
 use the lib:
 ------------------------------------------------------------
 local MergeMarkdown = NPL.load("MergeMarkdown");
+-- output file in "temp/MergedMarkdown.md"
+-- example 1:
 local mmd = MergeMarkdown:new():Init("https://keepwork.com/lixizhi/lessons/books/paracraft01")
-mmd:SaveAs("temp/MergedMarkdown.md")
+
+-- example 2:
+local mmd = MergeMarkdown:new():Init("books/paracraft01.md")
 ------------------------------------------------------------
 ]]
 
-local MergeMarkdown = commonlib.inherit(nil, NPL.export())
+local MergeMarkdown = commonlib.inherit(commonlib.gettable("System.Core.ToolBase"), NPL.export())
 
+MergeMarkdown:Signal("finished")
 
 function MergeMarkdown:ctor()
 	self.files = {}
 end
 
 function MergeMarkdown:Init(root_url)
+	self.root_url = root_url;
+	self.co = coroutine.create(function()
+		self:Parse()
+		self:SaveAs();
+		self:finished();
+	end)
+	coroutine.resume(self.co);
 end
 
 -- @param address: like "/lixizhi/lessons/books/paracraft" or "https://keepwork.com/lixizhi/lessons/books/paracraft"
@@ -37,14 +49,81 @@ function MergeMarkdown:GetRawUrlFromAddress(address)
     if(address:match("%.md$")) then
         return address;
     end
-    address = address:gsub("%https?://[^/]&", "")
+    address = address:gsub("^https?://([^/]+)", "")
 
     local userName, webName = address:match("^/([^/]+)/([^/]+)");
     if (userName and webName)  then
         -- https://git.keepwork.com/gitlab_rls_lixizhi/keepworklessons/raw/master/lixizhi/lessons/books/6_future_edu.md
         local url = format("https://git.keepwork.com/gitlab_rls_%s/keepwork%s/raw/master%s.md", userName, webName, address)
+		return url;
     end
+	return address;
 end
 
+-- @param address: url or file 
+-- return text or nil
 function MergeMarkdown:GetWikiContent(address)
+	local url = self:GetRawUrlFromAddress(address);
+	if(url:match("^http")) then
+		LOG.std(nil, "info", "MergeMarkdown", "fetching %s", url)
+		System.os.GetUrl(url, function(err, msg, data)
+			if(err == 200) then
+				coroutine.resume(self.co, nil, data);
+			else
+				LOG.std(nil, "error", "MergeMarkdown", "%s failed with %d", address, err)
+				coroutine.resume(self.co, nil);
+			end
+		end);
+		local err, data = coroutine.yield();
+		return data;
+	else
+		local file = ParaIO.open(url, "r")
+		if(file) then
+			local data = file:GetText(0, -1)
+			file:close();
+			return data;
+		end
+	end
+end
+
+-- private: 
+function MergeMarkdown:Parse()
+	local o = {};
+	self:AddContent(self.root_url, o)
+	self.output = table.concat(o, "\n");
+end
+
+function MergeMarkdown:AddContent(url_, o)
+	local text = self:GetWikiContent(url_);
+	if(text) then
+		for line in text:gmatch("([^\r\n]*)\r?\n?") do
+			local inline = line:match("^<<(.*)")
+			local url;
+			if(inline) then
+				url = inline:match("%[[^%]]*%]%(([^%)]+)%)")
+				if(not url) then
+					if(inline:match("^http")) then
+						url = inline;
+					end
+				end
+			end
+			if(url) then
+				self:AddContent(url, o)
+			else
+				o[#o+1] = line;
+			end
+		end
+	end
+end
+
+function MergeMarkdown:SaveAs(filename)
+	filename = filename or "temp/MergedMarkdown.md"
+	if(self.output) then
+		local file = ParaIO.open(filename, "w");
+		if(file) then
+			file:WriteString(self.output);
+			LOG.std(nil, "info", "MergeMarkdown", "%s is generated", filename)
+			file:close();
+		end
+	end
 end
